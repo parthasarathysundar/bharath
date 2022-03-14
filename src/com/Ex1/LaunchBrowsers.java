536b1d26efeb14cc56fa6b9d460803dab1a555e9
parthasarathysundar <parthasarathysundar30@gmail.com>
2022-03-14 10:08:19 +0530

    project

---
+++ src/com/Ex1/LaunchBrowsers.java
@@ -0,0 +1,9 @@
+package com.Ex1;
+
+public class LaunchBrowsers {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\partha\\Downloads\\chromedriver_win32");
+		
+	}
+
+}
